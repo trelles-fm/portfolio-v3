91f40415a303032bef99015929e4772eed4dd65e
trelles-fm <franco.trellesm@gmail.com>
2023-03-24 16:41:19 -0400

    add dev tools & setup styling directory

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { AnimatePresence } from 'framer-motion';
+import './index.css';
+
+const handleExitComplete = () => {
+  if (typeof window !== 'undefined') {
+    window.scrollTo({ top: 0 });
+  }
+};
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <AnimatePresence mode="wait" onExitComplete={handleExitComplete}>
+      <App />
+    </AnimatePresence>
   </React.StrictMode>,
-)
+);
