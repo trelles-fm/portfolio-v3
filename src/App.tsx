c25127513dbb8f8d86bd51cd4e0fe3a2fa063e77
trelles-fm <franco.trellesm@gmail.com>
2023-03-26 22:28:01 -0400

    add heading section & main section

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header>
+        <div>
+          <h1>transforming creative ideas into code</h1>
+        </div>
+      </header>
+      <main>
+        <p>
+          Hello! 👋, I am a graphic designer turned full-stack engineer, always enthusiastic about building solutions,
+          tackling challenges, and writing clean, efficient code. My design experience gives me a boost in creating
+          user-friendly and visually appealing software.
+        </p>
+      </main>
+    </div>
+  );
 }
 
 export default App;
