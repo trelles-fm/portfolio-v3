729e44b8bfa559c321142918977f425e820d86c9
trelles-fm <franco.trellesm@gmail.com>
2023-03-31 23:55:07 -0400

    add header font, update scss vars, begin navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
-import './App.css';
+import '../styles/global.scss';
+import { Logo } from './components/Logo';
 
 function App() {
   return (
     <div className="App">
+      <nav>
+        <Logo />
+      </nav>
       <header>
         <div>
-          <h1>transforming creative ideas into code</h1>
+          <h1 className="main-header">
+            TRANSFORMING
+            <br />
+            <span>
+              CREATIVE <span>IDEAS</span>
+              <br />
+              INTO CODE.
+            </span>
+          </h1>
+          <p className="sub-header">Franco Trelles, full-stack software engineer based in New York</p>
         </div>
       </header>
       <main>
         <p>
-          Hello! 👋, I am a graphic designer turned full-stack engineer, always enthusiastic about building solutions,
-          tackling challenges, and writing clean, efficient code. My design experience gives me a boost in creating
-          user-friendly and visually appealing software.
+          Hello! 👋, I am a graphic designer turned full-stack engineer, enthusiastic about building solutions, tackling
+          challenges, and writing clean, efficient code. My design experience gives me a boost in creating user-friendly
+          and visually appealing software.
         </p>
       </main>
     </div>
